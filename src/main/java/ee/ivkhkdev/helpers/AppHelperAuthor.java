621e2a03ab585a025dbbff60d0067e39bb57708e
TeacherIVKHK <teacherivkhk@ivkhk.ee>
2024-10-29 11:11:22 +0200

    Дописали тесты для различных случаев добавления авторов

--- src/main/java/ee/ivkhkdev/helpers/AppHelperAuthor.java
+++ src/main/java/ee/ivkhkdev/helpers/AppHelperAuthor.java
@@ -34,7 +34,7 @@ public class AppHelperAuthor implements AppHelper<Author> {
     @Override
     public boolean printList(List<Author> authors) {
         try {
-            if(authors.size() == 0) return false;
+            if(authors.size() < 1) return false;
             for(int i = 0; i < authors.size(); i++){
                 System.out.printf("%d. %s %s%n", i+1,authors.get(i).getFirstname(),authors.get(i).getLastname());
             }
